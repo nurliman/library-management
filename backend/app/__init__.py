e0ecb3f7a2f853a9ab92db572a1693c690e22586
nurliman <nurlimandiara@gmail.com>
2024-01-23 20:20:56 +0700

    Add python-dotenv package and load .env file

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,6 +5,9 @@ from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Base(DeclarativeBase):
